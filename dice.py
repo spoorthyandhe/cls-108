c6890a3f06067508bd240066c057f9a059f7072d
spoorthyandhe <70134338+spoorthyandhe@users.noreply.github.com>
2021-08-15 11:49:55 +0530

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,16 @@
+import random
+import plotly.express as px
+import plotly.figure_factory as ff
+
+count=[]
+dice_result=[]
+
+for i in range (0,100):
+    dice1=random.randint(1,6)
+    dice2=random.randint(1,6)
+    dice_result.append(dice1+dice2)
+    count.append(i)
+
+fig=ff.create_distplot([dice_result],["result"])
+fig.show()    
+
